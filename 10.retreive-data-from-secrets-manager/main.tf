df5be4ac73e6f3f2e14df12bb179bfb6c6ac574e
gospodinqodirov1 <gospodinqodirov1@gmail.com>
2022-10-18 07:02:11 +0000

    [feat:] implement aws ssm param store and retreive data as json

--- 10.retreive-data-from-secrets-manager/main.tf
+++ 10.retreive-data-from-secrets-manager/main.tf
@@ -25,51 +25,84 @@ resource "aws_db_instance" "rds_instance" {
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
   username             = "administrator"
-  password             = data.aws_secretsmanager_secret_version.rds_password_version.secret_string
+  password             = data.aws_secretsmanager_secret_version.rds_password.secret_string
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
 }
 
 # Generating random complex password
 resource "random_password" "main" {
-    length = 20
-    special = true
-    override_special = "!#()_"
+  length           = 20
+  special          = true
+  override_special = "!#()_"
 }
 
 # Store Generetaed Password in SSM Param
-resource "aws_secretsmanager_secret" "password" {
-    name = "/prod/prod-mysql-rds/password"
-    description = "Master DB Password for RDS Database"
-    recovery_window_in_days = 0
+resource "aws_secretsmanager_secret" "rds_password" {
+  name                    = "/prod/rds/password"
+  description             = "Master DB Password for RDS Database"
+  recovery_window_in_days = 0
 }
 
-resource "aws_secretsmanager_secret_version" "password" {
-  secert_id = aws_secretsmanager_secret.password.id
+# Store All Parameters as json in SSM Param
+resource "aws_secretsmanager_secret" "rds" {
+  name                    = "/prod/rds/all"
+  description             = "All Params for RDS Database"
+  recovery_window_in_days = 0
+}
+
+resource "aws_secretsmanager_secret_version" "rds_password" {
+  secret_id     = aws_secretsmanager_secret.rds_password.id
   secret_string = random_password.main.result
+}
+
+resource "aws_secretsmanager_secret_version" "rds" {
+  secret_id = aws_secretsmanager_secret.rds.id
+  secret_string = jsonencode({
+    rds_endpoint           = aws_db_instance.rds_instance.address,
+    rds_rds_port           = aws_db_instance.rds_instance.port,
+    rds_username           = aws_db_instance.rds_instance.username,
+    rds_master_db_password = random_password.main.result
+  })
 }
 
 
 # Retreive Password SSM
-data "aws_secretsmanager_secret_version" "rds_password_version" {
-  secret_id = aws_secretsmanager_secret.password.id
-  depend_on = [aws_secretsmanager_secret_version.password]
+data "aws_secretsmanager_secret_version" "rds_password" {
+  secret_id  = aws_secretsmanager_secret.rds_password.id
+  depends_on = [aws_secretsmanager_secret_version.rds_password]
 }
+
+# Retrevice All data from SSM
+data "aws_secretsmanager_secret_version" "rds" {
+  secret_id  = aws_secretsmanager_secret.rds.id
+  depends_on = [aws_secretsmanager_secret_version.rds]
+}
+
 
 #------------------------Output Section----------------------------#
 output "rds_endpoint" {
-    value = aws_db_instance.rds_instance.address
+  value = aws_db_instance.rds_instance.address
 }
 output "rds_rds_port" {
-    description = "RDS instance port"
-    value = aws_db_instance.rds_instance.port
+  description = "RDS instance port"
+  value       = aws_db_instance.rds_instance.port
 }
 output "rds_username" {
-    description = "RDS instance root username"
-    value = aws_db_instance.rds_instance.username
-    sensitive   = true
+  description = "RDS instance root username"
+  value       = aws_db_instance.rds_instance.username
+  sensitive   = true
 }
 output "rds_master_db_password" {
-    value = data.aws_ssm_parameter.rds_password.value
-    sensitive   = true
+  value     = aws_secretsmanager_secret.rds_password.id
+  sensitive = true
+}
+
+
+output "rds_all" {
+  value = jsondecode(
+    data.aws_secretsmanager_secret_version.rds.secret_string
+  )
+  sensitive = true
+
 }
